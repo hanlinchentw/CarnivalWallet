28028f20e5a17b3aa3647f8640bd680ddb4d26fb
陳翰霖 <chenhanlin@Hans-MacBook-Pro.local>
2022-12-01 13:45:28 +0800

    feat: add coin view

---
+++ CarnivalWallet/Feature/CoinPage/CoinView.swift
@@ -0,0 +1,20 @@
+//
+//  CoinView.swift
+//  CarnivalWallet
+//
+//  Created by 陳翰霖 on 2022/12/1.
+//
+
+import SwiftUI
+
+struct CoinView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoinView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinView()
+    }
+}
